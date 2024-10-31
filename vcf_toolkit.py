d95d49bb46879d77cecc01b67eebb3ae42faedfe
Laura <laurarias.nunez@gmail.com>
2024-10-31 18:12:47 +0100

    Uso de pandas, subprocess, y creacion de nchrom nsamples y filter

--- vcf_toolkit.py
+++ vcf_toolkit.py
@@ -2,12 +2,14 @@
 ## creo que la línea de arriba permite que el archivo se ejecute como
 ## un script de python sin especificar el intérprete
 
+# Se ejecuta como ./vcf_toolkit.py search -input archivo.vcf
 
 ## Importacion de argparse para trabajar en linea de comandos
 
 import argparse
 import sys
-
+import subprocess
+import pandas as pd
 
 ## Definicion clase vcf_toolkit.py
 
@@ -41,13 +43,11 @@ class vcf_toolkit:
 
 
     def search_position(self, position):
-        position_sep = position.split(':')
-        ocurrencias=''
-        for l in self.lineas:
-            linea_sep = l.split()
-            if position_sep[0]==linea_sep[0] and position_sep[1]==linea_sep[1]:
-                ocurrencias += (l +'\n')
-        return ocurrencias
+        try:
+            ocurrencias = subprocess.run(f'bcftools view -H -r {position} {self.archivo}.gz'.split(), capture_output=True, text=True)
+            return ocurrencias
+        except FileNotFoundError:
+            return 'Necesitas comprimir el archivo .vcf antes utilizando bzgip para que el programa pueda encontrarlo en el mismo directorio en el que está el archivo original'
     
     def chrom(self, chr):
         ocurrencias=''
@@ -56,7 +56,14 @@ class vcf_toolkit:
             if linea_sep[0]==chr:
                 ocurrencias += (l+ '\n')
         return ocurrencias
+    
+    def columnas(self):
+        header = subprocess.run(f'bcftools view -h {self.archivo}'.split(), capture_output=True, text=True) #para coger el header
+        header_columnas = header.stdout.strip().split('\n')[-1] #para ver la última fila del header
+        header_columnas_lista = header_columnas.replace('#','').split('\t') #para crear una lista con todos los elementos de la última fila del header
+        return header_columnas_lista
 
+    
 
 
 ## cuerpo del programa que se ejecutará si el script está siendo ejecutado directamente
@@ -68,9 +75,9 @@ def body():
                                      nrows_nh: Devuelve el número de filas en el archivo sin tener en cuenta el header\n\
                                      search: Devuelve las filas en las que aparece la posición introducida (chrx:xxxxxxxxx)\n\
                                      chrom: Devuelve las filas del cromosoma introducido\n\
-                                     chrom_n: Devuelve el número de filas por cromosoma\n\
+                                     nchrom: Devuelve el número de filas por cromosoma\n\
                                      nsamples: Devuelve el número de muestras\n\
-                                     failpass: Devuelve la proporción de FAIL y PASS")
+                                     filter: Devuelve el número de FAIL PASS o .")
 
     subparsers = parser.add_subparsers(dest="command", help="Subcomandos disponibles")
 
@@ -88,11 +95,21 @@ def body():
     parser_chrom.add_argument("-input", help="Direccion del archivo vcf")
     parser_chrom.add_argument("-chr", help="Cromosoma que se quiere buscar")
 
+    parser_nsamples = subparsers.add_parser("nsamples", help="Devuelve el número de muestras en el archivo")
+    parser_nsamples.add_argument("-input", help="Direccion del archivo vcf")
+
+    parser_nchrom = subparsers.add_parser("nchrom", help="Cuenta el número de filas de cada cromosoma")
+    parser_nchrom.add_argument("-input", help="Direccion del archivo vcf")
+
+    parser_filter = subparsers.add_parser("filter", help="Cuenta los FAIL y PASS de filter")
+    parser_filter.add_argument("-input", help="Dirección del archivo vcf")
+
     args = parser.parse_args()
 
     #
 
     vcf = vcf_toolkit(args.input) #se crea un objeto de la clase vcf_toolkit con la direccion del archivo dado
+    vcf_panda = pd.read_csv(vcf.archivo, comment="#", sep="\t", header=None, names=vcf.columnas())
 
     if args.command=="nrows":
         output = vcf.nrows()
@@ -102,6 +119,13 @@ def body():
         output = vcf.search_position(args.position)
     elif args.command=="chrom":
         output = vcf.chrom(args.chr)
+    elif args.command=="nsamples":
+        ncolumnas = len(vcf.columnas())
+        output = ncolumnas -9 #Siempre hay 9 columnas de datos y el resto de muestras o puede variar????
+    elif args.command=="nchrom":
+        output = vcf_panda['CHROM'].value_counts()
+    elif args.command=="filter":
+        output = vcf_panda['FILTER'].value_counts()
 
 
     
