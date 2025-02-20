1e65682f85a7edbca1922c9f22c8adb8abc7cef0
laura-an <laurarias.nunez@gmail.com>
2025-02-20 20:31:38 +0100

    Delete test_call.py

--- test_call.py
+++
@@ -1,7 +0,0 @@
-from vcf_toolkit_simple import *
-
-from glob import glob
-print(glob('*'))
-
-#vcf_0filas = vcf_toolkit('/home/laura/icm/vcf_toolkitRepository/vcf0filas.vcf')
-#print(vcf_0filas.nrows())
