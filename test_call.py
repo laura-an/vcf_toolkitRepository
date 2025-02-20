7ecd3ccba86718805e313b257c986276bb03789e
Laura <laurarias.nunez@gmail.com>
2025-02-20 20:29:09 +0100

    aceptar borrar test_call.py

---
+++ test_call.py
@@ -0,0 +1,7 @@
+from vcf_toolkit_simple import *
+
+from glob import glob
+print(glob('*'))
+
+#vcf_0filas = vcf_toolkit('/home/laura/icm/vcf_toolkitRepository/vcf0filas.vcf')
+#print(vcf_0filas.nrows())
